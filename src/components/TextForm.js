1ba5df475aed18ada33f037580bfe8bd8cafc60a
umarbsse <umarbsse@gmail.com>
2024-04-26 12:50:08 +0500

    Video 8 done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,14 +6,18 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handleLoClick = () => {
+    //console.log("Uppercase button wah clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
   const handleOnChange = (event) => {
-    //console.log("On change");
     setText(event.target.value);
   };
-  const [text, setText] = useState("Enter Text Here");
-  //setText ("abc");
+  const [text, setText] = useState("");
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">
@@ -26,9 +30,20 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>
         Convert to uppercase
       </button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+        Convert to Lower case
+      </button>
     </div>
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008*text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
