00a62f18bc528fd4bd4980120d2a6b536f60f974
umarbsse <umarbsse@gmail.com>
2024-04-28 00:36:57 +0500

    before routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-//import About from "./components/About";
+import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 import React, { useState } from "react";
+
+
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -21,11 +23,19 @@ function App() {
     if(mode==='light'){
       setMode('dark')
       document.body.style.backgroundColor='#03105a';
-      showAlert("Dark mode has been enabled","success")
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'Textutills - Darkmode';
+      /*setInterval(() => {
+        document.title = 'Textutills - Buy me';
+      }, 2000);
+      setInterval(() => {
+        document.title = 'Install Textutills please';
+      }, 1500);*/
     }else{
       setMode('light');
       document.body.style.backgroundColor='white';
       showAlert("Light mode has been enabled","success")
+      document.title = 'Textutills - Lightmode';
     }
   }
   return (
@@ -33,10 +43,12 @@ function App() {
       <Navbar title="TextUtils" aboutTitle="About" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert}/>
       <div className="container my-3">
-        {/*<About/>*/}
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} />
+      <TextForm showAlert={showAlert} heading="Enter the text to analyze"  mode={mode} />
+      {/*<About/>*/}
       </div>
     </>
+
+
   );
 }
 
