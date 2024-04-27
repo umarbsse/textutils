e3d71384dd7543979649e16090035f4821045d4f
umarbsse <umarbsse@gmail.com>
2024-04-27 23:21:11 +0500

    video 14 done

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -63,7 +63,7 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3"  style={{color:props.mode==='dark'?'white':'#03105a'}}>
       <h2>Your text summary</h2>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{text.length===0?'0':text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008*text.split(" ").length} Minutes read</p>
       <h2>Preview</h2>
       <p>{text.length>0?text:"Enter your text into textarea to preview here."}</p>
